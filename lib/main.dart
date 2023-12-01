7723a92a0a54eb326d16bfbffc1396a8b8a55770
pablo <mulekinrx1v9@gmail.com>
2023-12-01 20:05:30 -0300

    segundo commit, navegabilidade entre telas

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:sem_23/models/usuario_models.dart';
+import 'package:logger/logger.dart';
+var logger = Logger();
 
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Usuario usuario = Usuario(); // Crie uma instância de Usuario
+
     return MaterialApp(
-      home: const HomeScreen(),
+      title: 'Meu App',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
       routes: {
-        // Correção: Remova 'const' ao criar a instância CadastroScreen
-        'Cadastro': (context) => _CadastroScreen(usuario: Usuario(avatarUrl: "https://imgs.search.brave.com/mKHvNlCdLPoHI5tdXTPvb9VTNoJwg3qL7TiIIBNUD1g/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/dmV0b3Jlcy1wcmVt/aXVtL2ZhbnRhc3Rp/Y28tZS1wb2Rlcm9z/by12ZXRvci1kZS1h/cnRlLWRvLWVtYmxl/bWEtdmlraW5nXzcy/Mjk2NC01Mi5qcGc_/c2l6ZT02MjYmZXh0/PWpwZw", id: '1',nome: "pablo",email: "pablo@gmail.com")), // Passe uma instância válida de Usuario aqui
-        'Login':(context) => _LoginTela(usuario: Usuario(avatarUrl: "https://imgs.search.brave.com/mKHvNlCdLPoHI5tdXTPvb9VTNoJwg3qL7TiIIBNUD1g/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/dmV0b3Jlcy1wcmVt/aXVtL2ZhbnRhc3Rp/Y28tZS1wb2Rlcm9z/by12ZXRvci1kZS1h/cnRlLWRvLWVtYmxl/bWEtdmlraW5nXzcy/Mjk2NC01Mi5qcGc_/c2l6ZT02MjYmZXh0/PWpwZw", id: '1',nome: "pablo",email: "pablo@gmail.com"),)
+        '/': (context) =>  _HomeScreen(),
+        '/tela_um': (context) => _TelaUm(usuario: usuario), // Corrija a rota // Passe o usuário para _TelaUm
+        'loja_ingressos': (context) => _LojaIngressoClasse(usuario: usuario), // Passe o usuário para _LojaIngressoClasse
       },
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+
+
+
+
+class _HomeScreen extends StatelessWidget {
+
+  
+ const _HomeScreen({Key? key, }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -31,81 +43,55 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Minha App'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, 'Cadastro');
+        child: 
+        ElevatedButton(
+      onPressed: () {
+     Navigator.pushNamed(
+              context,
+              '/tela_um');
           },
-          child: const Text('Ir para Cadastro'),
-        ),
+  child: const Text('Ir para Tela Um'),
+),
       ),
     );
   }
 }
 
-class _CadastroScreen extends StatefulWidget {
-  final Usuario usuario;
+class _TelaUm extends StatelessWidget {
+  final Usuario usuario; // Declare a variável aqui
 
-  const _CadastroScreen({Key? key, required this.usuario}) : super(key: key);
-
-  @override
-  _CadastroScreenState createState() => _CadastroScreenState();
-}
-
-class _CadastroScreenState extends State<_CadastroScreen> {
-  final _formKey = GlobalKey<FormState>();
-  String? _novoEmail;
-  String? _novoNome;
-  String? _novaAvatarUrl;
+  // Construtor que recebe o usuário
+  const _TelaUm({Key? key, required this.usuario}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tela de Cadastro'),
+        title: const Text("Menu"),
+        backgroundColor: Colors.grey[800],
       ),
-      body: Form(
-        key: _formKey,
+      body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'insira seu email'),
-              onChanged: (novoEmail) {
-                setState(() {
-                  _novoEmail = novoEmail;
-                });
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'insira seu nome de usuário'),
-              onChanged: (novoNome) {
-                setState(() {
-                  _novoNome = novoNome;
-                });
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(labelText: 'cole uma url para ser sua imagem de avatar'),
-              onChanged: (novaAvatarUrl) {
-                setState(() {
-                  _novaAvatarUrl = novaAvatarUrl;
-                });
-              },
-            ),
             ElevatedButton(
               onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  // Lógica para enviar os dados do formulário para o objeto usuario
-                  widget.usuario.receberEmail(_novoEmail!);
-                  widget.usuario.receberNome(_novoNome!);
-                  widget.usuario.receberAvatarUrl(_novaAvatarUrl!);
-
-                  // Navegar de volta para a tela anterior
-                  Navigator.pop(context);
-                  //lógica para seguir para tela de login
-                  Navigator.pushNamed(context, 'Login');
-                }
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => _LojaIngressoClasse(usuario: usuario),
+                  ),
+                );
               },
-              child: const Text('Enviar'),
+              style: ElevatedButton.styleFrom(
+                primary: const Color.fromARGB(68, 31, 116, 213),
+                padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                textStyle: const TextStyle(fontSize: 24),
+              ),
+              child: const Text(
+                "Ir para loja de ingressos",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           ],
         ),
@@ -114,63 +100,121 @@ class _CadastroScreenState extends State<_CadastroScreen> {
   }
 }
 
-class _LoginTela extends StatelessWidget {
-  final Usuario usuario;
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _nomeController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
 
-  _LoginTela({required this.usuario, Key? key}) : super(key: key);
+
+class _LojaIngressoClasse extends StatelessWidget {
+  final Usuario usuario; // Adicione o usuário como propriedade
+
+  const _LojaIngressoClasse({required this.usuario});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Minha App'),
+        title: const Text('Loja de Ingressos'),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _nomeController,
-              decoration: const InputDecoration(labelText: 'Insira seu nome de usuário'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor, insira seu nome de usuário';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(labelText: 'Insira seu email'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Por favor, insira seu email';
-                }
-                return null;
-              },
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  final novoNome = _nomeController.text;
-                  final novoEmail = _emailController.text;
-
-                  if (usuario.validarEmail(novoEmail) && usuario.validarNome(novoNome)) {
-                    // Os dados do formulário são válidos e correspondem aos dados armazenados no objeto usuario
-                    // Faça o que for necessário aqui
-                  } else {
-                    // Exibir uma mensagem de erro ou tomar outra ação se os dados não forem válidos
-                  }
-                }
-              },
-              child: const Text('Enviar'),
-            ),
-          ],
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8.0,
+          mainAxisSpacing: 8.0,
         ),
+        itemCount: usuario.tipoUsuario == TipoUsuario.cliente
+            ? usuario.favoritos.length // Substitua por seus dados reais
+            : 0,
+        itemBuilder: (BuildContext context, int index) {
+          Ingresso ingresso = usuario.favoritos[index]; // Substitua por seus dados reais
+
+          return GestureDetector(
+            // onTap: () {
+            //   if (usuario.tipoUsuario == TipoUsuario.cliente) {
+            //     // Se o usuário for um cliente, navegue para a página TelaComprar
+            //     Navigator.push(
+            //       context,
+            //       MaterialPageRoute(
+            //         builder: (context) => _TelaComprar(ingresso: ingresso),
+            //       ),
+            //     );
+            //   }
+            // },
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Colors.grey[200],
+              ),
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    ingresso.imagem,
+                    height: 100.0,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(
+                    ingresso.descricao,
+                    style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  ),
+                  Text('R\$ ${ingresso.valorIngresso.toString()}'),
+                  // Adicione o Checkbox
+                  if (usuario.tipoUsuario == TipoUsuario.cliente)
+                    Checkbox(
+                      value: usuario.favoritos.contains(ingresso),
+                      onChanged: (value) {
+                        // Lógica para adicionar/remover dos favoritos do cliente
+                        usuario.marcarDesmarcarFavorito(ingresso, value);
+                      },
+                    ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
+
+
+
+// class _TelaUsuario extends StatelessWidget {
+//   final Usuario usuario;
+
+//   const _TelaUsuario({required this.usuario});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     List<Ingresso> ingressosComprados = usuario.getIngressosComprados();
+
+//     // Agora você pode usar a lista 'ingressosComprados' para renderizar a interface do usuário
+//     // e permitir que o usuário clique em cada ingresso.
+//     // Exemplo: use um ListView.builder para criar um botão para cada ingresso.
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Ingressos Comprados'),
+//       ),
+//       body: ListView.builder(
+//         itemCount: ingressosComprados.length,
+//         itemBuilder: (BuildContext context, int index) {
+//           Ingresso ingresso = ingressosComprados[index];
+//           return ListTile(
+//             title: Text(ingresso.descricao),
+//             // Adicione aqui a lógica para navegar para os detalhes do ingresso
+//             onTap: () {
+//               // Exemplo: navegar para a tela de detalhes do ingresso
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) => DetalhesIngresso(ingresso: ingresso),
+//                 ),
+//               );
+//             },
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
--- lib/models/usuario_models.dart
+++ lib/models/usuario_models.dart
@@ -1,39 +1,119 @@
-import 'dart:io';
+import 'package:logger/logger.dart';
+var logger = Logger();
+enum TipoUsuario { cliente, fornecedor }
+
 class Usuario {
-  final String id;
-  String avatarUrl;
-  String nome;
-  String email;
+  final String? id; // id é uma String no MongoDB
+  final TipoUsuario? tipoUsuario;
+  double saldo;
+  final String nome;
+  final String email;
+  final List<Ingresso> ingressosComprados;
+  final List<String> convites;
+  final List<Ingresso> favoritos;
 
   Usuario({
-    required this.id,
-    required this.avatarUrl,
-    required this.nome,
-    required this.email,
+    this.id,
+    this.tipoUsuario,
+    this.saldo = 0.0,
+    this.nome = '',
+    this.email = '',
+    List<Ingresso>? ingressosComprados,
+    this.convites = const [],
+    List<Ingresso>? favoritos,
+  })  : ingressosComprados = ingressosComprados ?? [],
+        favoritos = favoritos ?? [];
+
+  // Método para cadastrar um novo ingresso
+  void cadastrarIngresso(Ingresso novoIngresso) {
+    if (tipoUsuario == TipoUsuario.fornecedor) {
+      // Lógica para fornecedor cadastrar um novo ingresso
+    } else {
+      logger.i("Apenas fornecedores podem cadastrar ingressos.");
+    }
+  }
+
+  // Método para comprar um ingresso
+  void comprarIngresso(Ingresso ingresso) {
+    if (tipoUsuario == TipoUsuario.cliente && saldo >= ingresso.valorIngresso) {
+      saldo -= ingresso.valorIngresso;
+      ingressosComprados.add(ingresso);
+      logger.i("Ingresso comprado com sucesso!");
+    } else {
+      logger.e("Usuário não é cliente ou não tem saldo suficiente.");
+    }
+  }
+
+        // como usar 
+        // fornecedor.cadastrarIngresso(novoIngresso);
+        // cliente.comprarIngresso(novoIngresso);
+
+
+      //retorna lista de ingressos comprados
+   List<Ingresso> getIngressosComprados() {
+    if (tipoUsuario == TipoUsuario.cliente) {
+      return List<Ingresso>.from(ingressosComprados);
+    } else {
+      // Se o usuário não for um cliente, retorna uma lista vazia
+      return [];
+    }
+  }
+
+  // Método para marcar ou desmarcar um ingresso como favorito
+  void marcarDesmarcarFavorito(Ingresso ingresso, bool? value) {
+    if (tipoUsuario == TipoUsuario.cliente) {
+      if (value == true) {
+        favoritos.add(ingresso);
+      } else {
+        favoritos.remove(ingresso);
+      }
+    } else {
+      logger.i("adicionado aos favoritos.");
+    }
+  }
+
+  // Método para retornar lista de favoritos marcados checkbox
+  List<Ingresso> getListaFavoritos() {
+    if (tipoUsuario == TipoUsuario.cliente) {
+      return List<Ingresso>.from(favoritos);
+    } else {
+      // Se o usuário não for um cliente, retorna uma lista vazia
+      return [];
+    }
+  }
+
+}
+
+class Ingresso {
+  final double valorIngresso;
+  final String imagem;
+  final String descricao;
+  final String dataEvento;
+  final String dataPostagem;
+  final String localEvento;
+  final String qrCode;
+
+  Ingresso({
+    required this.valorIngresso,
+    required this.imagem,
+    required this.descricao,
+    required this.dataEvento,
+    required this.dataPostagem,
+    required this.localEvento,
+    required this.qrCode,
   });
 
-  // Método para receber e configurar o e-mail
-  void receberEmail(novoEmail) {
-    email = stdin.readLineSync()!;
-  }
+  Ingresso.fromMap(Map<String, dynamic> map)
+      : valorIngresso = map['valorIngresso'],
+        imagem = map['imagem'],
+        descricao = map['descricao'],
+        dataEvento = map['dataEvento'],
+        dataPostagem = map['dataPostagem'],
+        localEvento = map['localEvento'],
+        qrCode = map['qrCode'];
 
-  // Método para receber e configurar o nome
-  void receberNome(novoNome) {
-    nome = stdin.readLineSync()!;
-  }
-
-  // Método para receber e configurar a URL do avatar
-  void receberAvatarUrl(novoUrl) {
-    avatarUrl = stdin.readLineSync()!;
-  }
-
-  bool validarEmail(String novoEmail) {
-    return novoEmail == email;
-  }
-
-  bool validarNome(String novoNome) {
-    return novoNome == nome;
-  }
-  
+        
 }
+
+
 
